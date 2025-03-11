0770c7e734cb3ba021099d59d13f3e28f37d6f1f
daro <dfghjk@email.com>
2025-03-11 09:17:30 +0100

    Dodanie funkcji pobierajacych dane z formularza

--- AnkietaPreferencji/MainWindow.axaml.cs
+++ AnkietaPreferencji/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace AnkietaPreferencji;
 
@@ -7,5 +10,18 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        SubmitButton.Click += Submit_button_Click;
+    }
+
+    private void Submit_button_Click(object sender, RoutedEventArgs e)
+    {
+        var valueimie = Imie.Text;
+        var valueprzedmiot = (Przedmiot.SelectedItem as ComboBoxItem) ?.Content?.ToString() ?? "Nie wybrane";
+        var selectedCheckbox = new[] {Tak1,Tak2,Tak3,Tak4,Tak5};
+        var count = selectedCheckbox.Count(p => p.IsChecked == true);
+        
+        Console.Write();
+        
+        
     }
 }
